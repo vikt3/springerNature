34502cd3c824ef5e7ad9396d76adaa87e0a22f7e
Viktorija Poderskyte <viktorijapoderskyte@gmail.com>
2017-09-10 23:57:58 +0300

    POM

--- src/test/java/testquidco.java
+++ src/test/java/testquidco.java
@@ -2,18 +2,57 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
 public class testquidco {
 
-	
+	private static WebDriver driver = null;
+
 	public static void main(String[] args) {
 
-		login A2 = new login();
-		A2.login(args);
+		// Setting chrome
+		WebDriver driver = new ChromeDriver();
+
+		String title = "";
+		driver.get("https://www.quidco.com/join-quidco");
+
+		// Closing unwanted dialog
+		signupForm.lnk_otherCountry(driver).click();
+
+		signupForm.lnk_Name(driver).sendKeys("Name");
+		signupForm.lnk_Lastname(driver).sendKeys("Lastname");
+		signupForm.lnk_Email(driver).sendKeys("viktorija+quidco1@gmail.com");
+		signupForm.lnk_Password(driver).sendKeys("Superpass");
+		signupForm.lnk_Terms(driver).click();
+		signupForm.lnk_Submit(driver).click();
+
+		title = driver.getTitle();
+		if (title.contentEquals("Quidco - The UK's #1 Cashback Site")) {
+			System.out.println("Test Passed");
+		} else {
+			System.out.println("Test failed");
+		}
 		
-		errors A1 = new errors();
-		A1.errorMessages(args);
-		
+
+		// Initiating new broswer window for another testcase
+		driver.get("https://www.quidco.com/join-quidco");
+
+		// Closing unwanted dialog
+		//signupForm.lnk_otherCountry(driver).click();
+
+		signupForm.lnk_Email(driver).sendKeys("email@");
+		signupForm.lnk_Password(driver).sendKeys("a");
+		signupForm.lnk_Submit(driver).click();
+
+		// Checking error messages
+		if (driver.getPageSource().contains("Email address is invalid")) {
+			System.out.println("Correct error messages for email");
+			if (driver.getPageSource().contains("Oops, that's not long enough")) {
+				System.out.println("Correct error messages for password");
+			}
+		} else {
+			System.out.println("Page does not work");
+		}
+
+		driver.quit();
 		System.out.println("Test passed");
 
 	}

ec24d42b976268c7e603e06d90b9dc2623967d83
Viktorija Poderskyte <viktorijapoderskyte@gmail.com>
2017-09-10 22:56:28 +0300

    Added comments

--- src/test/java/errors.java
+++ src/test/java/errors.java
@@ -4,44 +4,43 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class errors {
 
-	public static void errorMessages(String[] args) {
-		
-	System.setProperty("chromedriver", "");
-	WebDriver driver=new ChromeDriver();
-	
-	String baseUrl = "https://www.quidco.com/join-quidco/";
-    String title = "";
-	driver.get(baseUrl);
-	
-	driver.findElement(By.xpath("//*[@id=\"modal-country\"]/div/div/div[3]/button")).click();
-	
-	   title = driver.getTitle();
-	    if (title.contentEquals("Join Quidco Now!")){
-	        System.out.println("Page works");
-	    } else {
-	        System.out.println("Page does not work");
-	        driver.close();
-	    }
-	  
-	    driver.findElement(By.id("new_email_join")).sendKeys("<script>badmail</script>");
-	    driver.findElement(By.id("new_password_join")).sendKeys("a");
-		
-	    driver.findElement(By.id("join-quidco-submit-button")).click();
+	public void errorMessages(String[] args2) {
 
-	 
-	    if(driver.getPageSource().contains("Email address is invalid"))
-	    {
-	    	System.out.println("Correct error messages for email");
-	    	if(driver.getPageSource().contains("Oops, that's not long enough")) 
-	    	{
-	    	System.out.println("Correct error messages for password");
-	    	}
-	    }
-	    else
-	    {
-	    	System.out.println("Page does not work");
-	    }
-	    
+		// Setting chrome
+		WebDriver driver = new ChromeDriver();
+
+		// Setting page url
+		String baseUrl = "https://www.quidco.com/join-quidco/";
+		String title = "";
+		driver.get(baseUrl);
+
+		// Closing unwanted dialog
+		driver.findElement(By.xpath("//*[@id=\"modal-country\"]/div/div/div[3]/button")).click();
+
+		// Checking if page opens
+		title = driver.getTitle();
+		if (title.contentEquals("Join Quidco Now!")) {
+			System.out.println("Page works");
+		} else {
+			System.out.println("Page does not work");
+			driver.close();
+		}
+
+		// Entering incorrect data
+		driver.findElement(By.id("new_email_join")).sendKeys("<script>badmail</script>");
+		driver.findElement(By.id("new_password_join")).sendKeys("a");
+		driver.findElement(By.id("join-quidco-submit-button")).click();
+
+		// Checking error messages
+		if (driver.getPageSource().contains("Email address is invalid")) {
+			System.out.println("Correct error messages for email");
+			if (driver.getPageSource().contains("Oops, that's not long enough")) {
+				System.out.println("Correct error messages for password");
+			}
+		} else {
+			System.out.println("Page does not work");
+		}
+		driver.quit();
 	}
-	
+
 }
--- src/test/java/testquidco.java
+++ src/test/java/testquidco.java
@@ -3,19 +3,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-
 public class testquidco {
 
 	
 	public static void main(String[] args) {
-		
-		
 
+		login A2 = new login();
+		A2.login(args);
 		
+		errors A1 = new errors();
+		A1.errorMessages(args);
 		
-	
-    
 		System.out.println("Test passed");
 
 	}
-	}
+}
